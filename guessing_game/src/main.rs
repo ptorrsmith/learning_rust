dec38798f2ff9a8e09854653309c986d39412886
Peter Torr Smith <peter@ptorrsmith.com>
2025-01-28 22:31:38 +1300

    exit after win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -43,7 +43,10 @@ fn main() {
         match guess.cmp(&the_secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
