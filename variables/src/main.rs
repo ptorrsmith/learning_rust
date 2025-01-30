dc2de4be76a65045695663446d93c57b7b3e92a2
Peter <peter@ptorrsmith.com>
2025-01-30 21:37:02 +1300

    vars let and const

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // println!("Hello, world!");
+    // const HELLO_WORLD: String = "Hello world";
+    // println!(HELLO_WORLD);
+
+    const ONE_HUNDRED: u32 = 10 * 10;
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = x * ONE_HUNDRED;
+    println!("The value of x is: {x}");
+}
