5c3c1dfaa36d676266a9095f84810e3d97374b64
Peter <peter@ptorrsmith.com>
2025-01-30 21:58:34 +1300

    shadowing by reusing let

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,30 @@
 fn main() {
-    // println!("Hello, world!");
-    // const HELLO_WORLD: String = "Hello world";
-    // println!(HELLO_WORLD);
+    let x = 5; // immutable
 
-    const ONE_HUNDRED: u32 = 10 * 10;
-    let mut x = 5;
     println!("The value of x is: {x}");
-    x = x * ONE_HUNDRED;
-    println!("The value of x is: {x}");
+
+    let x = x + 1; // could not re-assign unless re let
+
+    println!("The value of x with one added is: {x}");
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x after the scope is: {x}");
 }
+
+
+
+// fn main() {
+//     // println!("Hello, world!");
+//     // const HELLO_WORLD: String = "Hello world";
+//     // println!(HELLO_WORLD);
+
+//     const ONE_HUNDRED: u32 = 10 * 10;
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = x * ONE_HUNDRED;
+//     println!("The value of x is: {x}");
+// }
