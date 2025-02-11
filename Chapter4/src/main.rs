86ee39330e3f2618bd693ac53d818fd0842870df
Peter <peter@ptorrsmith.com>
2025-02-11 16:21:31 +1300

    ch4 wip

--- Chapter4/src/main.rs
+++ Chapter4/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let s = String::from("Hello world");
-    let s_ref = &s;
-    let s2 = *s_ref;
-    println!("{s2}");
-}
+    fn copy_to_prev(v: &mut Vec<i32>, i: usize) {
+        let n = &mut v[i];
+        *n = v[i - 1];
+      }
+      fn main() {
+        let mut v = vec![1, 2, 3];
+        copy_to_prev(&mut v, 1);
+      }
+    }
